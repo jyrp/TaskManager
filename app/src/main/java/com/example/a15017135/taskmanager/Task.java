c4225f18d5629e5263f3d5cfc9fab5bb1b756168
15017135 <15017135@myrp.edu.sg>
2017-05-30 12:31:43 +0800

    layout

--- app/src/main/java/com/example/a15017135/taskmanager/Task.java
+++ app/src/main/java/com/example/a15017135/taskmanager/Task.java
@@ -1,8 +1,10 @@
 package com.example.a15017135.taskmanager;
 
+import java.io.Serializable;
+
 /**
  * Created by 15017135 on 30/5/2017.
  */
 
-public class Task {
+public class Task implements Serializable{
 }
