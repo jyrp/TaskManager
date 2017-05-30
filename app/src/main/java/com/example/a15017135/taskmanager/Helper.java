bef5c7e6c08740332fa5e38ddc424b057dbf72f9
15017135 <15017135@myrp.edu.sg>
2017-05-30 13:07:04 +0800

    helper

---
+++ app/src/main/java/com/example/a15017135/taskmanager/Helper.java
@@ -0,0 +1,86 @@
+package com.example.a15017135.taskmanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 15017135 on 30/5/2017.
+ */
+
+public class Helper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "task.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_TASK = "task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_NAME = "_name";
+    private static final String COLUMN_DESCRIPTION = "_description";
+
+    public Helper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNoteTableSql = "CREATE TABLE " + TABLE_TASK + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT," + COLUMN_DESCRIPTION + " TEXT ) ";
+        db.execSQL(createNoteTableSql);
+        for (int i = 0; i< 4; i++) {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_NAME, "Data name " + i);
+            values.put(COLUMN_DESCRIPTION, "Data desc" + i);
+            db.insert(TABLE_TASK, null, values);
+        }
+        Log.i("info", "dummy records inserted");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        onCreate(db);
+    }
+
+    public long insertTask(String name,String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_DESCRIPTION, description);
+        long result = db.insert(TABLE_TASK, null, values);
+        if (result == -1) {
+            Log.d("DBHelper", "Insert failed");
+        }
+        db.close();
+        Log.d("SQL Insert",""+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+
+    public ArrayList<String> getAllTasks(){
+        ArrayList<String> tasks = new ArrayList<String>();
+
+        String selectQuery = "SELECT " + COLUMN_ID + ","
+                + COLUMN_NAME + "," + COLUMN_DESCRIPTION + " FROM " + TABLE_TASK;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String description = cursor.getString(2);
+                tasks.add("ID:" + id + ", " + name + ", " + description);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return tasks;
+    }
+
+
+
+}
--- app/src/main/java/com/example/a15017135/taskmanager/SQLiteOpenHelper.java
+++
@@ -1,8 +0,0 @@
-package com.example.a15017135.taskmanager;
-
-/**
- * Created by 15017135 on 30/5/2017.
- */
-
-public class SQLiteOpenHelper {
-}
